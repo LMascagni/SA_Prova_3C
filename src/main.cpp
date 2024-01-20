fc1701014ad6d732fa234c64c450097de85e42ce
fontana-m <126956465+fontana-m@users.noreply.github.com>
2024-01-20 13:22:40 +0100

    v1.0 - campionamento a 10Hz con delay

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,58 @@
 #include <Arduino.h>
 #include <SPI.h>
 
-struct main
+const int CS_PIN = 5;
+const int MOSI_PIN = 23;
+const int MISO_PIN = 19;
+const int CLK_PIN = 18;
+
+struct Channels
 {
+  float channel_0;
   float channel_1;
   float channel_2;
   float channel_3;
-  float channel_4;
 };
 
-const int CS_PIN = 15;
-const int MOSI_PIN = 13;
-const int MISO_PIN = 12;
-const int CLK_PIN = 14;
+Channels channels;
 
-int readADC(int channel) {
-  // MCP3204 formato: 0b0000XXX0, dove XXX è il numero del canale
-  byte command = B11000000 | (channel << 4);
+int readADC(uint16_t channel)
+{
+  int rawValue2;
+  byte control = 0b00000110;
+  channel <<= 14;
 
   digitalWrite(CS_PIN, LOW);
-  delayMicroseconds(1); 
-  int rawValue = SPI.transfer(command) & 0x0F;
-  rawValue <<= 8;
-  rawValue |= SPI.transfer(0);
+  delayMicroseconds(1);
+  SPI.transfer(control);
+  rawValue2 = SPI.transfer16(channel);
   digitalWrite(CS_PIN, HIGH);
-
-  return rawValue;
+  return rawValue2;
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
+  SPI.begin();
+  SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
 
   pinMode(CS_PIN, OUTPUT);
-  digitalWrite(CS_PIN, HIGH); 
-
-  SPI.begin();
+  digitalWrite(CS_PIN, HIGH);
 }
 
-void loop() {
-  for (int channel = 0; channel < 4; ++channel) {
+void loop()
+{
+  for (uint16_t channel = 0; channel < 4; ++channel)
+  {
     int rawValue = readADC(channel);
-    float tension = map(rawValue, 0, 4095, 0, 5);
-
+    float tension = ((float)rawValue/4095.0)*3.3;
     Serial.print("Canale ");
     Serial.print(channel);
     Serial.print(": Lettura grezza = ");
     Serial.print(rawValue);
     Serial.print(", Tensione = ");
     Serial.print(" V");
-    Serial.println(tension);
-
-    delay(1000);  //campionamento ogni secondo
+    Serial.println(tension,4);
   }
+  delay(50); // campionamento 20Hz
 }
