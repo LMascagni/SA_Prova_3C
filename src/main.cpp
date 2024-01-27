132072f3c04d823dc151978a98410636325efab8
Mascagni Lorenzo <Mascagni.Lorenzo@isisbassafriulana.edu.it>
2024-01-27 19:08:17 +0100

    commento del codice

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,16 @@
+/*
+  Nome del Programma: SA_Prova_3C
+  Autori: -Bianchin Francesco
+          -Fontana Matteo
+          -Gratton Claudio
+          -Mascagni Lorenzo
+          -Zorat Fabio
+  Scopo del Programma:
+  La ESP32 si collega in SPI con un ADC a 4 canali MCP3204, che ha risoluzione di 12bit.
+  Il programma acquisisce le tensioni dei 4 canali (ciascuno condizionato nell'intervallo da 0 a 3.3V) con velocità di campionamento di 10 campionamenti al secondo.
+  I dati vengono emessi sul terminale seriale per essere visualizzati con TelePlot in VS Code.
+*/
+
 #include <Arduino.h>
 #include <SPI.h>
 
@@ -43,7 +56,7 @@ void loop()
     v[channel] = ((float)rawValue/4095.0)*3.3;
   }
   for(int channel=0; channel<N_CHANNELS; channel++){
-    Serial.println("ch" + String(channel) + ": " + String(v[channel]));
+    Serial.println(">ch" + String(channel) + ": " + String(v[channel]));
   }
    
   delayMicroseconds(100); // campionamento 20Hz
