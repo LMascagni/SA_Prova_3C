9a4a3e8b9236cae382138d2a97ae93da3b6232e4
34346 <34346@ITTCV-SIS-09>
2024-02-26 11:24:33 +0100

    aiuto

--- src/main.cpp
+++ src/main.cpp
@@ -14,26 +14,36 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <UDT/data.h>
+#include <Wifi.h>
+#include <WifiUdp.h>
 
 #define N_CHANNELS 4
+
+#define WIFI_TIMEOUT_MS 10000       // 20 second WiFi connection timeout
+#define WIFI_RECOVER_TIME_MS 1000 // Wait 30 seconds after a failed connection attempt
 
 const int CS_PIN = 5;
 const int MOSI_PIN = 23;
 const int MISO_PIN = 19;
 const int CLK_PIN = 18;
 
+unsigned int localPort = 9999;
+const char *ssid = "Pisello";
+const char *password = "caccacacca";
+
 static const uint8_t msgQueueLen = 100; // dimensione della coda di comunicazione tra ISR e stampa
 static QueueHandle_t msg_queue;
 volatile i16Data campione; // un campionamento dei 4 canali della ISR
 
-// per ora inutile
-int frequency = 8000; // in Hz, numero di campionamenti al secondo
-
 hw_timer_t *timer0 = NULL;
+
+WiFiUDP udp;
 
 void IRAM_ATTR readADC();
 
 void printTask(void *parameters);
+
+void keepWiFiAlive(void *parameter);
 
 void setup()
 {
@@ -56,20 +66,29 @@ void setup()
 
   // creazione della coda per la comunicazione tra ISR e stampa dati
   msg_queue = xQueueCreate(msgQueueLen, sizeof(i16Data));
-
-  // creazione ed avvio del task di stampa
+  /*
+    // creazione ed avvio del task di stampa
+    xTaskCreatePinnedToCore(
+      printTask,
+      "Print Task",
+      2048,
+      NULL,
+      1,
+      NULL,
+      PRO_CPU_NUM
+    );
+  */
   xTaskCreatePinnedToCore(
-    printTask,
-    "Print Task",
-    2048,
-    NULL,
-    1,
-    NULL,
-    PRO_CPU_NUM
-  );
+      keepWiFiAlive,
+      "keepWiFiAlive",
+      5000,
+      NULL,
+      1,
+      NULL,
+      ARDUINO_RUNNING_CORE);
 
   // avvio del timer per il triggering della ISR
-  timerAlarmEnable(timer0);
+ // timerAlarmEnable(timer0);
 }
 
 void loop()
@@ -81,7 +100,7 @@ void IRAM_ATTR readADC()
 {
   BaseType_t xHigherPriorityTaskWoken;
 
-  //BaseType_t task_woken = pdFALSE;
+  // BaseType_t task_woken = pdFALSE;
 
   uint16_t rawValue;
   byte control = 0b00000110;
@@ -125,14 +144,13 @@ void IRAM_ATTR readADC()
 
   if (xQueueSendFromISR(msg_queue, (void *)&campione, &xHigherPriorityTaskWoken) != pdTRUE)
   {
-    
   }
-/*
-  if (xHigherPriorityTaskWoken)
-  {
-    portYIELD_FROM_ISR();
-  }
-*/
+  /*
+    if (xHigherPriorityTaskWoken)
+    {
+      portYIELD_FROM_ISR();
+    }
+  */
 }
 
 void printTask(void *parameters)
@@ -152,11 +170,14 @@ void printTask(void *parameters)
       Serial.print(">ch3:");
       Serial.println(rawValue.ch3);
 
-      Serial.println(uxQueueSpacesAvailable(msg_queue ));
+      Serial.println(uxQueueSpacesAvailable(msg_queue));
 
-      if(!uxQueueSpacesAvailable(msg_queue )){
+      if (!uxQueueSpacesAvailable(msg_queue))
+      {
         timerAlarmDisable(timer0);
-      }else{
+      }
+      else
+      {
         timerAlarmEnable(timer0);
       }
     }
@@ -164,5 +185,36 @@ void printTask(void *parameters)
     {
       // Serial.println("attesa dati...");
     }
+  }
+}
+
+void keepWiFiAlive(void *parameter)
+{
+  for (;;)
+  {
+    if (WiFi.status() == WL_CONNECTED)
+    {
+      vTaskDelay(10000 / portTICK_PERIOD_MS);
+      continue;
+    }
+
+    Serial.println("[WIFI] Connecting");
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+
+    unsigned long startAttemptTime = millis();
+
+    while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < WIFI_TIMEOUT_MS)
+    {
+    }
+
+    if (WiFi.status() != WL_CONNECTED)
+    {
+      Serial.println("[WIFI] FAILED");
+      vTaskDelay(WIFI_RECOVER_TIME_MS / portTICK_PERIOD_MS);
+      continue;
+    }
+
+    Serial.println("[WIFI] Connected: " + WiFi.localIP());
   }
 }
